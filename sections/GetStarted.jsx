ddf899a512feb8ee3a4cd6b5f81bde79cb85feb0
aditi27-rajshail <aditirajankhot.7@gmail.com>
2023-02-01 15:57:43 +0530

    initial commit

---
+++ sections/GetStarted.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { motion } from "framer-motion";
+
+import { TypingText, TitleText, StartSteps } from "../components";
+import styles from "../styles";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import { startingFeatures } from "../constants";
+const GetStarted = () => <section>Get Started Section</section>;
+
+export default GetStarted;
